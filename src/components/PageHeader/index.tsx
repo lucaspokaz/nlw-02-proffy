edad0086fe67c252784546f227db9acf8d438b5b
Lucas Magalhães <lucaspokaz@gmail.com>
2020-08-07 13:55:35 -0300

    alterando layout

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg'
-import backIcon from '../../assets/images/icons/back.svg'
+import backIcon from '../../assets/images/icons/back2.svg'
 import './styles.css'
 
 interface PageHeaderProps {
@@ -14,7 +14,7 @@ const PageHeader: React.FC<PageHeaderProps> = (props) => {
     <header className="page-header">
       <div className="top-bar-container">
         <Link to="/">
-          <img src={backIcon} alt="voltar" />
+          <img className="botao-voltar" src={backIcon} alt="voltar" />
         </Link>
         <img src={logoImg} alt="proffy" />
       </div>
